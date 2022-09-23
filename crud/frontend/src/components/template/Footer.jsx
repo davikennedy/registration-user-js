81f8d2ace29366b294c72c4bd2f2231ee2304bd5
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-09-22 22:08:26 -0300

    Configuring component: UserCrud :white_check_mark:

--- crud/frontend/src/components/template/Footer.jsx
+++ crud/frontend/src/components/template/Footer.jsx
@@ -4,6 +4,6 @@ import React from 'react'
 export default props =>
     <footer className="footer">
         <span>
-            Desenvolvido com <i className='fa fa-heart text-danger'></i> por <strong>Davi Kennedy</strong>
+            Desenvolvido com <i className='fa fa-coffee'></i> por <strong>Davi Kennedy</strong>
         </span>
     </footer>
